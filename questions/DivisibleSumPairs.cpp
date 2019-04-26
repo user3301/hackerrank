bfaaa83fa5c539c104af29b8117c9e1ccdd3ef69
user3301 <stan_gai@Hotmail.com>
2019-04-26 17:05:17 +1000

    divisible sum pair (optimized solution with o(n) complexity)

--- questions/DivisibleSumPairs.cpp
+++ questions/DivisibleSumPairs.cpp
@@ -2,16 +2,41 @@
 // Created by user3301 on 2019-04-25.
 //
 #include <vector>
+//#define loop(i,n) for(int i = 0; i < int(n); ++i)
 class solution
 {
 public:
-    int divisbleSumPairs(int n, int k, std::vector<int> ar)
+    // brutal force try every combination
+//    int divisibleSumPairs(int n, int k, std::vector<int> ar)
+//    {
+//        int ans = 0;
+//        for (int i = 0; i < n; ++i) {
+//            for (int j = i+1; j <n ; ++j) {
+//                if((ar[i]+ar[j])%k==0) ++ans;
+//            }
+//        }
+//        return ans;
+//    }
+
+    // O(N) optimized solution
+    int divisibleSumPairs(int n , int k, std::vector<int> ar)
     {
-        int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = i+1; j <n ; ++j) {
-                if((ar[i]+ar[j])%k==0) ++ans;
+        std::vector<int> remainsTable(k, 0);
+
+        for(int i = 0; i < n; ++i)
+        {
+            ++remainsTable[ar[i]%k];
+        }
+
+        int ans = remainsTable[0] * (remainsTable[0]-1)/2;
+
+        for (int j = 1; j < remainsTable.size(); ++j) {
+            if(j == k-j)
+            {
+                ans += remainsTable[j] * (remainsTable[j]-1)/2;
             }
+            else if(j < k-j) ans += remainsTable[j] * remainsTable[k-j];
+            else break;
         }
         return ans;
     }
--- tests/DivisibleSumPairsTest.cpp
+++ tests/DivisibleSumPairsTest.cpp
@@ -14,6 +14,24 @@ TEST_F(DivisibleSumPairsTest, testmethod1)
     std::vector<int> ar{1,3,2,6,1,2};
     int n = 6, k = 3;
     int expected = 5;
-    int actual = solution.divisbleSumPairs(n, k, ar);
+    int actual = solution.divisibleSumPairs(n, k, ar);
+    ASSERT_EQ(expected, actual);
+}
+
+TEST_F(DivisibleSumPairsTest, testmethod2)
+{
+    std::vector<int> ar{1,2,3,4,5,6};
+    int n = 6, k = 3;
+    int expected = 5;
+    int actual = solution.divisibleSumPairs(n, k, ar);
+    ASSERT_EQ(expected, actual);
+}
+
+TEST_F(DivisibleSumPairsTest, testmethod3)
+{
+    std::vector<int> ar{5,9,10,7,4};
+    int n = 5, k = 2;
+    int expected = 4;
+    int actual = solution.divisibleSumPairs(n, k, ar);
     ASSERT_EQ(expected, actual);
 }
