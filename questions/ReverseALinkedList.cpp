2b2be45d0a2744638d7715ada97628f4b6dd3fc2
zhelun <zhelun.gai@tmacomms.com>
2019-05-10 09:59:34 +1000

    reverse linked list

--- questions/ReverseALinkedList.cpp
+++ questions/ReverseALinkedList.cpp
@@ -1,4 +1,21 @@
 //
 // Created by user3301 on 5/9/2019.
 //
+#include "../common/SinglyLinkedListNode.h"
+class ReverseALinkedList {
+public:
+    //iterative way
+    SinglyLinkedListNode* reverse(SinglyLinkedListNode* head) {
+        SinglyLinkedListNode* pre = nullptr;
+        SinglyLinkedListNode* cur = head;
+        SinglyLinkedListNode* next;
 
+        while(cur!= nullptr) {
+          next = cur->next;
+          cur->next = pre;
+          pre = cur;
+          cur = next;
+        }
+        return pre;
+    }
+};
