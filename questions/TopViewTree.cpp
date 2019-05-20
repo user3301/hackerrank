0c95e4d20946bab0ae70639fc3114414e6055125
zhelun <zhelun.gai@tmacomms.com>
2019-05-20 22:00:50 +1000

    tree top view

--- questions/TopViewTree.cpp
+++ questions/TopViewTree.cpp
@@ -1,61 +1,56 @@
 #include "../common/TreeNode.h"
-#include <unordered_set>
+#include <unordered_map>
 #include <iostream>
 #include <queue>
 #include <vector>
 //
 // Created by user3301 on 5/14/2019.
 //
-class TreeNodeWithPosition {
-public:
-    int position;
-    TreeNode* node;
-
-    TreeNodeWithPosition(TreeNode* node, int position) {
-        this->position = position;
-        this->node = node;
-    }
-};
 
 class TopViewTree {
 public:
     void topView(TreeNode* root) {
-       std::queue<TreeNodeWithPosition*> q;
-       std::vector<TreeNodeWithPosition*> nodes{};\
-       std::unordered_set<int> pos;
+      if(root == nullptr) return;
+      std::unordered_map<TreeNode*, int> map;
+      std::queue<TreeNode*> q;
+      std::vector<TreeNode*> list;
+      map.emplace(root, 0);
+      list.push_back(root);
+      int low = 0;
+      int high = 0;
+      q.emplace(root);
 
-       auto current = new TreeNodeWithPosition(root, 0);
-       q.emplace(current);
+      while(q.size()!=0) {
+          int size = q.size();
+          for (int i = 0;
+               i < size; ++i) {
+              auto current = q.front();
+              q.pop();
+              if(current->left!=nullptr) {
+                  q.emplace(current->left);
+                  map.emplace(current->left, map.find(current)->second -1);
+                  if(low > (map.find(current)->second -1) || high < (map.find(current)->second -1)) {
+                      low = map.find(current)->second-1;
+                      list.insert(list.begin(), current->left);
+                  }
+              }
+              if(current->right != nullptr) {
+                  q.emplace(current->right);
+                  map.emplace(current->right, map.find(current)->second + 1);
+                  if(high < (map.find(current)->second +1) || low > (map.find(current)->second +1)) {
+                      high = map.find(current)->second +1;
+                      list.push_back(current->right);
+                  }
+              }
 
-       while(q.size()!=0) {
-           int size = q.size();
-           for(int i = 0; i<size; ++i) {
-               auto current = q.front();
-               q.pop();
 
-               nodes.push_back(current);
-               if(current->node->left !=nullptr) {
-                   auto left = new TreeNodeWithPosition(current->node->left, current->position -1);
-                   q.emplace(left);
-                   nodes.push_back(left);
-
-               }
-               if(current->node->right != nullptr) {
-                    auto right = new TreeNodeWithPosition(current->node->right, current->position +1);
-                    q.emplace(right);
-                    nodes.push_back(right);
-               }
-           }
-       }
-
-       for(auto x: nodes) {
-           if(pos.find(x->position)==pos.end()) {
-               std::cout<< x->node->data<<" ";
-               pos.emplace(x->position);
-           }
-       }
+          }
+      }
+        for(auto x:list) {
+            std::cout<<x->data<<" ";
+        }
     }
 
-};
+    };
 
 
--- tests/TopViewTreeTest.cpp
+++ tests/TopViewTreeTest.cpp
@@ -23,5 +23,19 @@ TEST_F(TopViewTreeTest, testmethod1) {
     node5->right = node6;
     node6->right = node7;
     topViewTree.topView(head);
+}
 
+TEST_F(TopViewTreeTest, testmethod2) {
+    TreeNode* head = new TreeNode(1);
+    TreeNode* node2 = new TreeNode(2);
+    TreeNode* node3 = new TreeNode(3);
+    TreeNode* node4 = new TreeNode(4);
+    TreeNode* node5 = new TreeNode(5);
+    TreeNode* node6 = new TreeNode(6);
+    head->right = node2;
+    node2->right = node5;
+    node5->left = node3;
+    node5->right = node6;
+    node3->right = node4;
+    topViewTree.topView(head);
 }
